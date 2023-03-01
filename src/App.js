88cfa2b090fbcbec5c92fd0fa04b40730e2cafd0
MeGallin <me@garyallin.uk>
2023-03-01 14:52:00 +0000

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import FooterComponent from './Footer/FooterComponent';
+import HeaderComponent from './Header/HeaderComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <HeaderComponent />
+      <h3>TEST</h3>
+      <FooterComponent />
     </div>
   );
 }
