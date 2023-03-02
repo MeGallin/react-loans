ac1b2af106c7c24f30c10414104596e8e018cafd
MeGallin <me@garyallin.uk>
2023-03-02 12:24:33 +0000

    basic graph working

--- src/components/AmortSchedule/AmortScheduleComponent.jsx
+++ src/components/AmortSchedule/AmortScheduleComponent.jsx
@@ -2,11 +2,21 @@ import { useState } from 'react';
 import './AmortScheduleComponent.css';
 import CurrencyFormat from 'react-currency-format';
 import InputComponent from '../Input/InputComponent';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
 
 const AmortScheduleComponent = () => {
-  const [amount, setAmount] = useState(0);
-  const [interestRate, setInterestRate] = useState(0);
-  const [period, setPeriod] = useState(0);
+  const [amount, setAmount] = useState(10000);
+  const [interestRate, setInterestRate] = useState(5);
+  const [period, setPeriod] = useState(36);
   const randId = (length) => {
     const chars =
       '0123456789abcdefghijklmnopqrstxyzABCDEFGHIJKLMNOPQRSTXYZ!"£$%^&*';
@@ -39,10 +49,10 @@ const AmortScheduleComponent = () => {
       schedule.push({
         id: randId(8),
         month: i,
-        payment: monthlyPayment.toFixed(2),
-        principal: principal.toFixed(2),
-        interest: interest.toFixed(2),
-        balance: balance.toFixed(2),
+        payment: Number(monthlyPayment.toFixed(2)),
+        principal: Number(principal.toFixed(2)),
+        interest: Number(interest.toFixed(2)),
+        balance: Number(balance.toFixed(2)),
       });
     }
     return schedule;
@@ -53,84 +63,141 @@ const AmortScheduleComponent = () => {
   };
   const schedule = calculateAmortizationSchedule(amount, interestRate, period);
 
+  const totalPMT = () => {
+    if (schedule) {
+      const payments = schedule.map((pmt) => pmt.payment);
+      return payments.reduce((acc, curr) => {
+        return acc + curr;
+      }, 0);
+    }
+  };
+  const totalIntPmt = () => {
+    if (schedule) {
+      const intPayments = schedule.map((int) => int.interest);
+      return intPayments.reduce((acc, curr) => {
+        return acc + curr;
+      }, 0);
+    }
+  };
+
   return (
-    <div style={{ overflowX: 'auto' }}>
-      <div className="input-wrapper">
-        <InputComponent
-          id="amount"
-          type="number"
-          label="amount"
-          value={amount}
-          onChange={handleAmount}
-        />
-        <InputComponent
-          id="interestRate"
-          type="number"
-          label="interest rate"
-          value={interestRate}
-          onChange={(e) => setInterestRate(e.target.value)}
-        />
-        <InputComponent
-          id="period"
-          type="number"
-          label="period months"
-          value={period}
-          onChange={(e) => setPeriod(e.target.value)}
-        />
+    <>
+      <div style={{ overflowX: 'auto' }}>
+        <div className="input-wrapper">
+          <InputComponent
+            id="amount"
+            type="number"
+            label="amount"
+            value={amount}
+            onChange={handleAmount}
+          />
+          <InputComponent
+            id="interestRate"
+            type="number"
+            label="interest rate"
+            value={interestRate}
+            onChange={(e) => setInterestRate(e.target.value)}
+          />
+          <InputComponent
+            id="period"
+            type="number"
+            label="period months"
+            value={period}
+            onChange={(e) => setPeriod(e.target.value)}
+          />
+        </div>
+
+        <div style={{ width: '100%', height: 360, margin: '10px auto' }}>
+          <ResponsiveContainer>
+            <LineChart
+              width={500}
+              height={300}
+              data={schedule}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="2 10" />
+              <XAxis dataKey="month" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="interest"
+                stroke="orange"
+                activeDot={{ r: 8 }}
+              />
+              <Line type="monotone" dataKey="payment" stroke="pink" />
+              <Line type="monotone" dataKey="principal" stroke="yellow" />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+
+        {!amount || !interestRate || !period ? null : (
+          <>
+            <div>
+              <h3>Summary</h3>
+              <div>TOTAL PAYMENTS {totalPMT().toFixed(2)}</div>
+              <div>TOTAL INTEREST PAYMENTS {totalIntPmt().toFixed(2)}</div>
+            </div>
+            <table>
+              <thead>
+                <tr>
+                  <th>MONTH</th>
+                  <th>PMT</th>
+                  <th>PRINCIPAL</th>
+                  <th>INTEREST</th>
+                  <th>BALANCE</th>
+                </tr>
+              </thead>
+              <tbody>
+                {schedule.map((amort) => (
+                  <tr key={amort.id}>
+                    <td>{amort.month}</td>
+                    <td>
+                      <CurrencyFormat
+                        value={amort.payment}
+                        displayType={'text'}
+                        thousandSeparator={true}
+                        prefix={''}
+                      />
+                    </td>
+                    <td>
+                      <CurrencyFormat
+                        value={amort.principal}
+                        displayType={'text'}
+                        thousandSeparator={true}
+                        prefix={''}
+                      />
+                    </td>
+                    <td>
+                      <CurrencyFormat
+                        value={amort.interest}
+                        displayType={'text'}
+                        thousandSeparator={true}
+                        prefix={''}
+                      />
+                    </td>
+                    <td>
+                      <CurrencyFormat
+                        value={amort.balance}
+                        displayType={'text'}
+                        thousandSeparator={true}
+                        prefix={''}
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </>
+        )}
       </div>
-      {!amount || !interestRate || !period ? null : (
-        <table>
-          <thead>
-            <tr>
-              <th>MONTH</th>
-              <th>PMT</th>
-              <th>PRINCIPAL</th>
-              <th>INTEREST</th>
-              <th>BALANCE</th>
-            </tr>
-          </thead>
-          <tbody>
-            {schedule.map((amort) => (
-              <tr key={amort.id}>
-                <td>{amort.month}</td>
-                <td>
-                  <CurrencyFormat
-                    value={amort.payment}
-                    displayType={'text'}
-                    thousandSeparator={true}
-                    prefix={''}
-                  />
-                </td>
-                <td>
-                  <CurrencyFormat
-                    value={amort.principal}
-                    displayType={'text'}
-                    thousandSeparator={true}
-                    prefix={''}
-                  />
-                </td>
-                <td>
-                  <CurrencyFormat
-                    value={amort.interest}
-                    displayType={'text'}
-                    thousandSeparator={true}
-                    prefix={''}
-                  />
-                </td>
-                <td>
-                  <CurrencyFormat
-                    value={amort.balance}
-                    displayType={'text'}
-                    thousandSeparator={true}
-                    prefix={''}
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+    </>
   );
 };
 
