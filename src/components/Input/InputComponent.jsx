a6ed2e9e43318d6c00af6ba5cc8875194fec16c5
MeGallin <me@garyallin.uk>
2023-03-02 10:07:53 +0000

    inputs

---
+++ src/components/Input/InputComponent.jsx
@@ -0,0 +1,41 @@
+import './InputComponent.css';
+import PropTypes from 'prop-types';
+
+const InputComponent = ({
+  label,
+  id,
+  type,
+  name,
+  value,
+  placeholder,
+  className,
+  onChange,
+}) => {
+  return (
+    <>
+      <div className="input-icon-wrapper">
+        {label && <label htmlFor={`input-field-${label}`}>{label}</label>}
+      </div>
+      <input
+        id={id}
+        type={type}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        className={className}
+        onChange={onChange}
+      />
+    </>
+  );
+};
+InputComponent.propTypes = {
+  label: PropTypes.string,
+  id: PropTypes.string,
+  type: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  placeholder: PropTypes.string,
+  className: PropTypes.string,
+  onChange: PropTypes.func,
+};
+export default InputComponent;
